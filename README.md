8ab635b3c14e8e534dc0793826009821cd151415
Bergsmf <bergsmf@outlook.com>
2025-06-18 21:47:41 +0100

    Envio acumulado. Funcionamlidades concluídas

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # TopLinguagens
+![Python](https://img.shields.io/badge/Python-3.12%2B-2a9d8f)
+![Pandas](https://img.shields.io/badge/Pandas-2.3.0-e9c46a)
+![Pytest](https://img.shields.io/badge/Pytest-8.4.0-e76f51)
+![Requests](https://img.shields.io/badge/Requests-2.32.4-f4a261)
+![Ruff](https://img.shields.io/badge/Ruff-0.11.13-8a4fff)
+![Streamlit](https://img.shields.io/badge/Streamlit-1.45.1-ff4b4b)
+![Taskipy](https://img.shields.io/badge/Taskipy-1.14.1-2e7d32)
+
+
+## Descrição
+Utilizei a API do site [Remotive](https://remotive.com/) para obter os dados sobre vagas disponíveis. Sabe quando quer tentar entender o mercado de trabalho? Quais as tecnologias estão sendo mais requisitades? O que precisa estudar além do requisito principal? Esse projeto tem como objetivo responder essas perguntas.
+
+### 1. Leitura dos dados:
+- Os dados são baixados através de um requisição pelo Requests;
+- Em seguida é salvo o arquivo vagas.json;
+
+### 2. Organização dos dados:
+- Como as vagas podem aceitar diferentes localizações e requisitar diversas tecnologias, os dados foram separados em 3 arquivos;
+- arq_vagas.parquet: São as informações gerais da vaga;
+- arq_locais.parquet: Um explode() das localizações aceitas de cada vaga;
+- arq_tecnologias.parquet: Um explode() dos requisitos técnicos da vaga;
+
+### 3. Visualização de Dados:
+- Foi utilizado o Streamlit para a visualização dos dados:
+
+### CI
+- A branch main foi bloqueada para push. Utilizo a branch develop e pull request. Além de uma etapa de CI:
+
+### Taskipy
+
+Coloquei no projeto o taskipy para facilitar a execução de comandos comuns durante o desenvolvimento. Abaixo estão as tarefas disponíveis:
+
+poetry run task <nome_da_tarefa>
+Tarefas disponíveis
+| Tarefa | Descrição |
+| --- | --- |
+| lint | Executa o linter ruff para verificar problemas de formatação e estilo. |
+| pre_format | Corrige automaticamente problemas detectados pelo ruff. |
+| format | Aplica o estilo de formatação definido (ruff format). |
+| pre_run | Executa o script main.py para processar e carregar os dados. |
+| run | Inicia a aplicação com Streamlit (toplinguagens/app.py). |
+| pre_test | Executa o lint automaticamente antes dos testes. |
+| test | Executa os testes usando pytest em modo verboso. |
+
+Observação: as tarefas pre_run e pre_test são chamadas automaticamente antes das tarefas run e test, respectivamente.
+
+## Estrutura de Diretórios
+<pre lang="markdown"><code>.
+.
+├── .github
+│   └── workflows
+│       └── CI.yaml
+├── README.md
+├── data
+│   ├── arq_locais.parquet
+│   ├── arq_tecnologias.parquet
+│   ├── arq_vagas.parquet
+│   └── vagas.json
+├── poetry.lock
+├── pyproject.toml
+├── tests
+│   └── test_vagas.py
+└── toplinguagens
+    ├── app.py
+    ├── classes
+    │   └── vagas.py
+    └── loader.py
+</code></pre>
+
+## Imagens
+
+### Vendas no mês
+![Vagas abertas por data](toplinguagens/img/Vagas_abertas_data.png)
+
+### Linguagens mais requisitadas
+![Mais requisitadas](toplinguagens/img/Principais_requisitos.png)
+
+### Produtos mais vendidos por mês
+![Filtro tecnologia](toplinguagens/img/Filtro_requisitos.png)
+![Filtro localizacao](toplinguagens/img/Filtro_localizacao.png)
+![Vagas Filtradas](toplinguagens/img/Vagas_filtradas.png)
---
+++ toplinguagens/app.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+
+import pandas as pd
+import streamlit as st
+
+
+def combinacoes(habilidade: str) -> list:
+    df = pd.read_parquet(Path.cwd() / 'data/arq_tecnologias.parquet')
+    list_id = df.loc[df['tags'] == habilidade, 'id'].unique()
+    list_tec = (
+        df[(df['id'].isin(list_id)) & (df['tags'] != habilidade)]['tags']
+        .value_counts()
+        .to_frame(name='ocorrencias')
+        .sort_values(by=['ocorrencias'], ascending=False)
+        .head(5)
+        .index.to_list()
+    )
+    return list_tec
+
+
+def obtem_datas():
+    st.title('Vagas disponíveis por data de abertura')
+    df_days = (
+        pd.read_parquet(Path.cwd() / 'data/arq_vagas.parquet')[
+            ['id', 'publication_date']
+        ]
+        .groupby('publication_date')['id']
+        .nunique()
+        .to_frame(name='qt_vagas')
+        .sort_values(by=['publication_date'])
+    )
+    st.line_chart(data=df_days['qt_vagas'], use_container_width=True)
+
+
+def tec_mais_requisitadas():
+    st.title('Habilidade mais requisitadas')
+    st.markdown('### E habilidades que são também pedidas  junto com elas')
+    df_habilidades = (
+        pd.read_parquet(Path.cwd() / 'data/arq_tecnologias.parquet')[['tags']]
+        .groupby('tags')
+        .value_counts()
+        .to_frame(name='ocorrencias')
+        .sort_values(by=['ocorrencias'], ascending=False)
+        .reset_index()
+        .head(10)
+    )
+    df_habilidades['combina_com'] = df_habilidades['tags'].apply(combinacoes)
+    pd.set_option('display.max_colwidth', None)
+    st.dataframe(df_habilidades, use_container_width=True)
+
+
+def busca_vaga():
+    st.title('Busque sua vaga')
+    df_lista_vagas = pd.read_parquet(
+        Path.cwd() / 'data/arq_tecnologias.parquet'
+    )
+    df_lista_vagas['tags'] = df_lista_vagas['tags'].apply(
+        lambda tec: tec.title() if isinstance(tec, str) else tec
+    )
+    df_lista_locais = pd.read_parquet(Path.cwd() / 'data/arq_locais.parquet')
+    df_lista_locais['candidate_required_location'] = df_lista_locais[
+        'candidate_required_location'
+    ].apply(lambda loc: loc.upper())
+    df_listas = pd.DataFrame(
+        df_lista_vagas['tags'].unique(), columns=['Tecnologia']
+    )
+    df_listas = df_listas.sort_values(by='Tecnologia').reset_index(drop=True)
+    tec_select = st.selectbox('Escolha sua tecnologia', df_listas)
+    local_select = st.radio(
+        'Escolha o local de atuação',
+        [
+            'Todos',
+            'Atuando do Brasil: Worldwide, Americas, LATAM, South America, '
+            'Brazil',
+            'Somente Brasil',
+        ],
+        index=0,
+    )
+    lista_id_vagas = (
+        df_lista_vagas[df_lista_vagas['tags'] == tec_select]['id']
+        .unique()
+        .tolist()
+    )
+    df_vagas = pd.read_parquet(Path.cwd() / 'data/arq_vagas.parquet')
+
+    match local_select:
+        case 'Todos':
+            lista_loc = df_lista_locais['id'].unique().tolist()
+        case (
+            'Atuando do Brasil: Worldwide, Americas, LATAM'
+            ', South America, Brazil'
+        ):
+            locais = [
+                'WORLDWIDE',
+                'AMERICAS',
+                'LATAM',
+                'SOUTH AMERICA',
+                'BRAZIL',
+            ]
+            lista_loc = (
+                df_lista_locais[
+                    df_lista_locais['candidate_required_location'].isin(locais)
+                ]['id']
+                .unique()
+                .tolist()
+            )
+        case 'Somente Brasil':
+            lista_loc = (
+                df_lista_locais[
+                    df_lista_locais['candidate_required_location'] == 'BRAZIL'
+                ]['id']
+                .unique()
+                .tolist()
+            )
+
+    df_tec_escolhida = df_vagas[
+        (df_vagas['id'].isin(lista_id_vagas))
+        & (df_vagas['id'].isin(lista_loc))
+    ]
+    if not (df_tec_escolhida.empty):
+        st.dataframe(df_tec_escolhida, use_container_width=True)
+    else:
+        st.write('Nenhuma vaga encontrada para esses filtros')
+
+
+if __name__ == '__main__':
+    obtem_datas()
+    tec_mais_requisitadas()
+    busca_vaga()
